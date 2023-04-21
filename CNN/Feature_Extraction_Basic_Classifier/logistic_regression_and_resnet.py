4eff03ad788248d410debd2f082f1047147aa992
dgulur <103679868+dgulur@users.noreply.github.com>
2023-04-21 10:32:34 -0400

    Update logistic_regression_and_resnet.py

--- CNN/Feature_Extraction_Basic_Classifier/logistic_regression_and_resnet.py
+++ CNN/Feature_Extraction_Basic_Classifier/logistic_regression_and_resnet.py
@@ -38,9 +38,9 @@ transform = transforms.Compose([
 
 # Load the pre-trained ResNet model and move it to GPU
 num_classes = 3
-net = resnet18(pretrained=True)
-num_features = net.fc.in_features
-net.fc = nn.Linear(num_features, num_classes)
+model = resnet18(pretrained=True).cuda()
+num_features = model.fc.in_features
+model.fc = nn.Linear(num_features, num_classes)
 # Set the model to evaluation mode
 model.eval()
 
